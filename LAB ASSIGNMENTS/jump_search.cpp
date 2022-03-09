eb752052fbc631d3f6f54c8521f0b241c3f93d95
vishalkr1030 <100745451+vishalkr1030@users.noreply.github.com>
2022-03-09 10:23:12 +0530

    Add files via upload

---
+++ LAB ASSIGNMENTS/jump_search.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int jump_search(int A[],int n,int key)
+{
+    int m=sqrt(n);
+    int check=0;
+    while(A[m-1]<key)
+    {
+        check=m;
+        m+=sqrt(n);
+        if(check>=n)
+        return -1;
+    }
+
+    while(A[check]<=key)
+    {
+        if(A[check]==key)
+            return check;
+        else
+            ++check;
+    }
+    return -1;
+} 
+int main()
+{
+    cout<<"ENTER SIZE OF ARRAY : ";
+    int n,key;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cout<<"ENTER ELEMENT AT INDEX "<<i+1<<" : "<<endl;
+        cin>>arr[i];
+    }
+    cout<<"Enter key : ";
+    cin>>key;
+    int index=jump_search(arr,n,key);
+    if(index==-1)
+    cout<<"ELEMENT DO NOT EXIST......................"<<endl;
+    else
+    cout<<"ELEMENT FOUND AT INDEX "<<index+1<<endl;
+}
