8684fbbb71efdd79ddc7d41e42a885c06df60d0a
VISHAL KUMAR SINGH <100745451+vishalkr1030@users.noreply.github.com>
2022-06-08 09:32:17 +0530

    ques3

--- LAB ASSIGNMENTS/week3/week3_ques3.cpp
+++ LAB ASSIGNMENTS/week3/week3_ques3.cpp
@@ -1,50 +1,69 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void Sel_Sort(int[], int);
+void merge(int arr[],int l,int mid,int h)
+{
+    int count=0;
+    int i=l,j=mid+1;
+    int temp[h-l+1];
+    int k=0;
+    
+    while (i<=mid && j<=h)
+    {
+        if (arr[i]<arr[j])
+            temp[k++]=arr[i++];
+        else
+        {
+            temp[k++]=arr[j++];
+            count+=mid-i+1;
+        }
+    }
+    for (;i<=mid;)
+         temp[k++]=arr[i++];
+         
+    for (;j<=h;)
+         temp[k++]=arr[j++];
+         
+    for (int f=0;f<k;f++)
+        arr[f+l]=temp[f];
+}
+void merge_sort(int arr[],int l,int h)
+{
+    if (l<h)
+    {
+        int mid=l+(h-l)/2;
+        merge_sort(arr,l,mid);
+        merge_sort(arr,mid+1,h);
+        merge(arr,l,mid,h);
+    }
+}
+
 
 int main()
 {
-    int T;
-    cin >> T;
-    for (int i = 0; i < T; i++)
+    int t;
+    cin>>t;
+    while (t--)
     {
         int n;
-        cin >> n;
-        int A[1000];
-        for (int j = 0; j < n; j++)
-        {
-            cin >> A[j];
-        }
+        cin>>n;
+        int arr[n];
+        for (int i=0;i<n;i++)
+           cin>>arr[i];
 
-        Sel_Sort(A, n);
-    }
-}
-void Sel_Sort(int A[], int n)
-{
-    int comp = 0, swaps = 0;
-    int min, temp = 0;
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        min = i;
-        for (int j = i + 1; j < n; j++)
+        merge_sort(arr,0,n-1);
+        int flag=0;
+        for (int i=0;i<n-1;i++)
         {
-            comp++;
-            if (A[min] > A[j])
-            {
-                min = j;
+            if (arr[i]==arr[i+1])
+            { 
+                cout<<"YES"<<endl;
+                flag=1;
             }
         }
-        swaps++;
-        swap(A[min], A[i]);
+        if (flag==0)
+            cout<<"NO"<<endl;
+         
     }
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << A[i] << " ";
-    }
-
-    cout << "\ncomparisons = " << comp << endl
-         << "swaps = " << swaps << endl;
 }
